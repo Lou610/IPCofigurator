7b06ef0080520820c06e6da0127cd544d7dd8a35
TheMeeper <lou@dvbytsolutions.com>
2023-08-13 09:09:14 +0200

    final project

---
+++ IPCinfigurator/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace IPCinfigurator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbxGateway = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCashier = new System.Windows.Forms.Label();
+            this.lblPOS = new System.Windows.Forms.Label();
+            this.lblDriver = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblClientname = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Browser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(269, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.comboBox1_DragDrop);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Client";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Configure IP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(292, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 26);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Reboot";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbxGateway
+            // 
+            this.tbxGateway.Location = new System.Drawing.Point(12, 105);
+            this.tbxGateway.Name = "tbxGateway";
+            this.tbxGateway.Size = new System.Drawing.Size(177, 20);
+            this.tbxGateway.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Default Gateway";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblCashier);
+            this.panel1.Controls.Add(this.lblPOS);
+            this.panel1.Controls.Add(this.lblDriver);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(177, 170);
+            this.panel1.TabIndex = 7;
+            // 
+            // lblCashier
+            // 
+            this.lblCashier.AutoSize = true;
+            this.lblCashier.Location = new System.Drawing.Point(66, 113);
+            this.lblCashier.Name = "lblCashier";
+            this.lblCashier.Size = new System.Drawing.Size(0, 13);
+            this.lblCashier.TabIndex = 13;
+            // 
+            // lblPOS
+            // 
+            this.lblPOS.AutoSize = true;
+            this.lblPOS.Location = new System.Drawing.Point(66, 77);
+            this.lblPOS.Name = "lblPOS";
+            this.lblPOS.Size = new System.Drawing.Size(0, 13);
+            this.lblPOS.TabIndex = 12;
+            // 
+            // lblDriver
+            // 
+            this.lblDriver.AutoSize = true;
+            this.lblDriver.Location = new System.Drawing.Point(66, 37);
+            this.lblDriver.Name = "lblDriver";
+            this.lblDriver.Size = new System.Drawing.Size(0, 13);
+            this.lblDriver.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Driver";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Cashier";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "POS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "District Details";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Client:";
+            // 
+            // lblClientname
+            // 
+            this.lblClientname.AutoSize = true;
+            this.lblClientname.Location = new System.Drawing.Point(39, 74);
+            this.lblClientname.Name = "lblClientname";
+            this.lblClientname.Size = new System.Drawing.Size(0, 13);
+            this.lblClientname.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(268, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(133, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Configure Router before IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 351);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblClientname);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxGateway);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IP Configurator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbxGateway;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCashier;
+        private System.Windows.Forms.Label lblPOS;
+        private System.Windows.Forms.Label lblDriver;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblClientname;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ IPCinfigurator/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using IPCinfigurator.Properties;
+
+namespace IPCinfigurator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        IPAddress gateway;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = true;
+
+            string newGateway = tbxGateway.Text;
+            string http = @"http://" + newGateway;
+            Process.Start(http);
+        }
+
+        public IPAddress GetDefaultGateway()
+        {
+
+            try
+            {
+                foreach (NetworkInterface f in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (f.OperationalStatus == OperationalStatus.Up)
+                    {
+                        foreach (GatewayIPAddressInformation d in f.GetIPProperties().GatewayAddresses)
+                        {
+                            gateway = d.Address;
+                        }
+                    }
+                }
+
+                return gateway;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("IP DOESNT EXIST");
+                return null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            ArrayList AR = new ArrayList();
+            comboBox1.Items.Insert(0, "-SELECT CLIENT-");
+            comboBox1.SelectedIndex = 0;
+            AR.Add("Gauteng Coaches");
+            AR.Add("Ntambanana");
+            AR.Add("Marblehall");
+            AR.Add("Mokopane");
+            AR.Add("UGU");
+            AR.Add("DUZI");
+
+            foreach (var item in AR)
+            {
+                comboBox1.Items.Add(item);
+            }
+
+            IPAddress gateWay = GetDefaultGateway();
+
+            tbxGateway.Text = gateWay.ToString();
+
+            if (tbxGateway.Text == "10.0.0.2")
+            {
+                lblClientname.Text = "Gauteng Coaches";
+                lblDriver.Text = "30";
+                lblPOS.Text = "29";
+                lblCashier.Text = "9";
+            }
+            else if (tbxGateway.Text == "192.168.24.1")
+            {
+                lblClientname.Text = "Ntambanana";
+                lblDriver.Text = "33";
+                lblPOS.Text = "34";
+                lblCashier.Text = "32";
+            }
+            else if (tbxGateway.Text == "192.168.8.1")
+            {
+                lblClientname.Text = "Marblehall";
+                lblDriver.Text = "22";
+                lblPOS.Text = "21";
+                lblCashier.Text = "7";
+            }
+            else if (tbxGateway.Text == "192.168.14.1")
+            {
+                lblClientname.Text = "Mokopane";
+                lblDriver.Text = "28";
+                lblPOS.Text = "27";
+                lblCashier.Text = "8";
+            }
+            else if (tbxGateway.Text == "10.10.10.254")
+            {
+                lblClientname.Text = "UGU";
+                lblDriver.Text = "26";
+                lblPOS.Text = "25";
+                lblCashier.Text = "5";
+            }
+            else if (tbxGateway.Text == "10.10.10.254")
+            {
+                lblClientname.Text = "DUZI";
+                lblDriver.Text = "24";
+                lblPOS.Text = "23";
+                lblCashier.Text = "6";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button3.Visible = true;
+            string ip;
+            string subnet;
+            string gateway;
+
+            if (comboBox1.SelectedItem == "Gauteng Coaches")
+            {
+                ip = "10.0.0.253";
+                subnet = "255.255.255.0";
+                gateway = "10.0.0.2";
+                setIP(ip, subnet);
+                setGateway(gateway);
+
+                //// check if file already exists in inboxbackup if it does then delete
+                if (File.Exists(Settings.Default.NewPath + Settings.Default.FileName))
+                {
+                    File.Delete(Settings.Default.NewPath + Settings.Default.FileName);
+                }
+                // copy file to backup folder - no "file already exists errors as check above has removed any existing files
+                File.Copy(Settings.Default.OldPath + Settings.Default.GautengCoaches, Settings.Default.NewPath + Settings.Default.FileName);
+            }
+            else if (comboBox1.SelectedItem == "Ntambanana")
+            {
+                ip = "192.168.24.253";
+                subnet = "255.255.255.0";
+                gateway = "192.168.24.1";
+                setIP(ip, subnet);
+                setGateway(gateway);
+                //// check if file already exists in inboxbackup if it does then delete
+                if (File.Exists(Settings.Default.NewPath + Settings.Default.FileName))
+                {
+                    File.Delete(Settings.Default.NewPath + Settings.Default.FileName);
+                }
+                // copy file to backup folder - no "file already exists errors as check above has removed any existing files
+                File.Copy(Settings.Default.OldPath + Settings.Default.Ntambanana, Settings.Default.NewPath + Settings.Default.FileName);
+            }
+            else if (comboBox1.SelectedItem == "Marblehall")
+            {
+                ip = "192.168.8.9";
+                subnet = "255.255.255.0";
+                gateway = "192.168.8.1";
+                setIP(ip, subnet);
+                setGateway(gateway);
+                //// check if file already exists in inboxbackup if it does then delete
+                if (File.Exists(Settings.Default.NewPath + Settings.Default.FileName))
+                {
+                    File.Delete(Settings.Default.NewPath + Settings.Default.FileName);
+                }
+                // copy file to backup folder - no "file already exists errors as check above has removed any existing files
+                File.Copy(Settings.Default.OldPath + Settings.Default.Marblehall, Settings.Default.NewPath + Settings.Default.FileName);
+            }
+            else if (comboBox1.SelectedItem == "Mokopane")
+            {
+                ip = "192.168.14.9";
+                subnet = "255.255.255.0";
+                gateway = "192.168.14.1";
+                setIP(ip, subnet);
+                setGateway(gateway);
+                // check if file already exists in inboxbackup if it does then delete
+                if (File.Exists(Settings.Default.NewPath + Settings.Default.FileName))
+                {
+                    File.Delete(Settings.Default.NewPath + Settings.Default.FileName);
+                }
+                // copy file to backup folder - no "file already exists errors as check above has removed any existing files
+                File.Copy(Settings.Default.OldPath + Settings.Default.Mokopane, Settings.Default.NewPath + Settings.Default.FileName);
+            }
+            else if (comboBox1.SelectedItem == "UGU")
+            {
+                ip = "10.10.10.253";
+                subnet = "255.255.255.0";
+                gateway = "10.10.10.254";
+                setIP(ip, subnet);
+                setGateway(gateway);
+                // check if file already exists in inboxbackup if it does then delete
+                if (File.Exists(Settings.Default.NewPath + Settings.Default.FileName))
+                {
+                    File.Delete(Settings.Default.NewPath + Settings.Default.FileName);
+                }
+                // copy file to backup folder - no "file already exists errors as check above has removed any existing files
+                File.Copy(Settings.Default.OldPath + Settings.Default.Marblehall, Settings.Default.NewPath + Settings.Default.FileName);
+            }
+            else if (comboBox1.SelectedItem == "DUZI")
+            {
+                ip = "10.10.10.253";
+                subnet = "255.255.255.0";
+                gateway = "10.10.10.254";
+                setIP(ip, subnet);
+                setGateway(gateway);
+                // check if file already exists in inboxbackup if it does then delete
+                if (File.Exists(Settings.Default.NewPath + Settings.Default.FileName))
+                {
+                    File.Delete(Settings.Default.NewPath + Settings.Default.FileName);
+                }
+                // copy file to backup folder - no "file already exists errors as check above has removed any existing files
+                File.Copy(Settings.Default.OldPath + Settings.Default.Duzi, Settings.Default.NewPath + Settings.Default.FileName);
+            }
+        }
+
+        public void setIP(string ip_address, string subnet_mask)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    try
+                    {
+                        ManagementBaseObject setIP;
+                        ManagementBaseObject newIP =
+                            objMO.GetMethodParameters("EnableStatic");
+
+                        newIP["IPAddress"] = new string[] { ip_address };
+                        newIP["SubnetMask"] = new string[] { subnet_mask };
+
+                        setIP = objMO.InvokeMethod("EnableStatic", newIP, null);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+
+
+                }
+            }
+        }
+
+        public void setGateway(string gateway)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    try
+                    {
+                        ManagementBaseObject setGateway;
+                        ManagementBaseObject newGateway =
+                            objMO.GetMethodParameters("SetGateways");
+
+                        newGateway["DefaultIPGateway"] = new string[] { gateway };
+                        newGateway["GatewayCostMetric"] = new int[] { 1 };
+
+                        setGateway = objMO.InvokeMethod("SetGateways", newGateway, null);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(Settings.Default.DeletePath);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+
+            if (MessageBox.Show("Reboot Computer", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Process.Start("shutdown", "/r /t 0"); // the argument /r is to restart the computer
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+            if (comboBox1.SelectedIndex == 1)
+            {
+                button1.Visible = true;
+                lblDriver.Text = "30";
+                lblPOS.Text = "29";
+                lblCashier.Text = "9";
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                button1.Visible = true;
+                lblDriver.Text = "33";
+                lblPOS.Text = "34";
+                lblCashier.Text = "32";
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                button1.Visible = true;
+                lblDriver.Text = "22";
+                lblPOS.Text = "21";
+                lblCashier.Text = "7";
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                button1.Visible = true;
+                lblDriver.Text = "28";
+                lblPOS.Text = "27";
+                lblCashier.Text = "8";
+            }
+            else if (comboBox1.SelectedIndex == 5)
+            {
+                button1.Visible = true;
+                lblDriver.Text = "26";
+                lblPOS.Text = "25";
+                lblCashier.Text = "5";
+            }
+            else if (comboBox1.SelectedIndex == 6)
+            {
+                button1.Visible = true;
+                lblDriver.Text = "24";
+                lblPOS.Text = "23";
+                lblCashier.Text = "6";
+            }
+        }
+
+        private void comboBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            button1.Visible = true;
+        }
+    }
+}
